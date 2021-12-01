b8f8978843195dbd716210a51f2bffc8f79889e7
Jungyeon <46320067+yeonns2@users.noreply.github.com>
2021-12-01 21:59:42 +0900

    Delete secret.js

--- config/secret.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    jwtsecret: 'MOA_jwt_secret_key',
-    AWS_ACCESS_KEY_ID: 'AKIA5GNCHEQDZ43D35OC', // AWS S3 액세스 키
-    AWS_SECRET_ACCESS_KEY: 'z6IL51njuu8NCkjttoehvofCFFbAD7d5rgf4o3ry', // AWS S3 액세스 시크릿 키
-    AWS_REGION: 'ap-northeast-2',
-};
